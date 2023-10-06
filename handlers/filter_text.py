79828087d8dc9c9aac48e8ce88bbaec539e518b2
d3xt3rwx <d3xt3rwx@gmail.com>
2023-10-07 00:07:11 +0300

    version-1.2-1

--- handlers/filter_text.py
+++ handlers/filter_text.py
@@ -5,23 +5,29 @@ from aiogram import Bot, Dispatcher, types, F
 
 router_filter = Router()
 
-
 SAVOCHKIN_PHRASE = [
-    'Савочкин', 'Savochkin', 'Савочкину', 'Савочкиным', 'Савочкін', 'Савочкіну', 'Савочкіним',
-    'Владислав Савочкін', 'Владислав Савочкин', 'Владиславу Савочкину', 'Влад Савочкин',
-    'савочкин', 'савочкін', 'владислав савочкін', 'владислав савочкин', 'савочкину', 'савочкина'
+    'савочкин', 'savochkin', 'савочкину', 'савочкиным', 'савочкін', 'савочкіну', 'савочкіним',
+    'владислав савочкін', 'владислав савочкин', 'владиславу савочкину', 'влад савочкин',
+    'савочкин', 'савочкін', 'владислав савочкін', 'владислав савочкин', 'савочкину', 'савочкина',
+    'савок'
 ]
 
 SKAKUN_PHRASE = [
-    'скакун', 'Скакун', 'Скакунчик', 'Артем Скакун', 'Skakun', 'Артём Скакун', 'Админ Скакун'
+    'скакун', 'скакунчик', 'артем скакун', 'skakun', 'артём скакун', 'админ скакун'
+]
+
+RAZE_PHRASE = [
+    'raze', 'рейз', 'рейзы'
 ]
 
 
 @router_filter.message()
 async def filter(message: types.Message):
-    if any(word in message.text for word in SAVOCHKIN_PHRASE):
+    if any(word.lower() in message.text.lower() for word in SAVOCHKIN_PHRASE):
         await message.reply("Савочкин пидрила ебаное. Мать его умерла давно.")
-    elif any(word in message.text for word in SKAKUN_PHRASE):
+    elif any(word.lower() in message.text.lower() for word in SKAKUN_PHRASE):
         await message.reply("Скакун пидрила ебаное. Мать его умерла давно.")
-    else:
-        pass
+    elif message.text.__contains__('@game_penis_bot'):
+        await message.reply('Ку)')
+    elif any(word.lower() in message.text.lower() for word in RAZE_PHRASE):
+        await message.reply('❤️ Рейзы - 100 ❤️')
